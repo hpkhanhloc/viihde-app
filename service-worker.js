d3546cbcc91bdf3727df5d8665d653c221bd2946
Loc Hoang <loc.hoang@aalto.fi>
2020-02-25 15:30:23 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://hpkhanhloc.github.io/viihde-app/precache-manifest.ed89510a3d0b46e6afa9fac263ea5be7.js"
+  "https://hpkhanhloc.github.io/viihde-app/precache-manifest.8d11eee436b9a16dd377a7e1602a1e16.js"
 );
 
 self.addEventListener('message', (event) => {
