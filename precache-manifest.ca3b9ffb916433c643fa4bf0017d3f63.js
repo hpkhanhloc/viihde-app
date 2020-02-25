c37cbc006be176739f302614afd1f800ac7d041a
Loc Hoang <loc.hoang@aalto.fi>
2020-02-25 14:11:38 +0700

    Updates

---
+++ precache-manifest.ca3b9ffb916433c643fa4bf0017d3f63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4a2afad58cff5888ec70eed188383b",
+    "url": "/viihde-app/index.html"
+  },
+  {
+    "revision": "b589274cdfb98701e82b",
+    "url": "/viihde-app/static/css/main.8913a5b7.chunk.css"
+  },
+  {
+    "revision": "b5af791b92e0fe9665ac",
+    "url": "/viihde-app/static/js/2.b22c37b8.chunk.js"
+  },
+  {
+    "revision": "6a43b004aa4e5442f43c089cbe637e64",
+    "url": "/viihde-app/static/js/2.b22c37b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b589274cdfb98701e82b",
+    "url": "/viihde-app/static/js/main.7a4f00a7.chunk.js"
+  },
+  {
+    "revision": "cf15d961cb9e17d7bbc6",
+    "url": "/viihde-app/static/js/runtime-main.d4112ef8.js"
+  }
+]);
